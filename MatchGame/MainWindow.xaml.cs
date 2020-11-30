ab38b187888d6adb6a4ab3bae1337e0f39be2e69
Ryan Antcliff <antcliffr12@gmail.com>
2020-11-29 19:24:46 -0500

    Added action to buttons and timer

--- MatchGame/MainWindow.xaml.cs
+++ MatchGame/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace MatchGame
 {
@@ -20,11 +21,28 @@ namespace MatchGame
     /// </summary>
     public partial class MainWindow : Window
     {
+        DispatcherTimer timer = new DispatcherTimer();
+        int tenthsOfSecondsElapsed;
+        int matchesFound;
         public MainWindow()
         {
             InitializeComponent();
 
+            timer.Interval = TimeSpan.FromSeconds(.1);
+            timer.Tick += Timer_Tick;
+
             SetUpGame();
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            tenthsOfSecondsElapsed++;
+            timeTextBlock.Text = (tenthsOfSecondsElapsed / 10F).ToString("0.0s");
+            if (matchesFound == 8)
+            {
+                timer.Stop();
+                timeTextBlock.Text = timeTextBlock.Text + " - Play again?";
+            }
         }
 
         private void SetUpGame()
@@ -46,10 +64,51 @@ namespace MatchGame
 
             foreach(TextBlock textBlock in mainGrid.Children.OfType<TextBlock>()) //Loop finds every TextBlock in the main grid and repeat the follwoing statements for each of them
             {
-                int index = random.Next(animalEmoji.Count); //Pick a random number between 0 and the number of emoji lef tin the list and call it "index"
-                string nextEmoji = animalEmoji[index]; //Use the random number called "index" to get a random emoji from the list
-                textBlock.Text = nextEmoji; //Update the TextBlock with the random emoji from the list
-                animalEmoji.RemoveAt(index); //Remove the random emoji from the list
+                if(textBlock.Name != "timeTextBlock")
+                {
+                    int index = random.Next(animalEmoji.Count); //Pick a random number between 0 and the number of emoji lef tin the list and call it "index"
+                    string nextEmoji = animalEmoji[index]; //Use the random number called "index" to get a random emoji from the list
+                    textBlock.Text = nextEmoji; //Update the TextBlock with the random emoji from the list
+                    animalEmoji.RemoveAt(index); //Remove the random emoji from the list
+                }
+               
+            }
+            timer.Start();
+            tenthsOfSecondsElapsed = 0;
+            matchesFound = 0;
+        }
+
+        TextBlock lastTextBlockClicked;
+        bool findingMatch = false; //keeps track of whether or not the player clicked on the first animal in a pair and is not trying to find match
+
+        private void TextBlock_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            TextBlock textBlock = sender as TextBlock;
+            if(findingMatch == false) //Player clicked the first animal in a pair, so it makes that animal hidden and keeps track of its TextBlock in case it needs to make it visible again
+            {
+                textBlock.Visibility = Visibility.Hidden;
+                lastTextBlockClicked = textBlock;
+                findingMatch = true;
+            }
+            else if(textBlock.Text == lastTextBlockClicked.Text) //The player found a match. Makes the second emoji invisible and resets findingMatch so the next animal clicked on is the first one in a pair again
+            {
+                matchesFound++;
+                textBlock.Visibility = Visibility.Hidden;
+                findingMatch = false;
+            }
+            else //Player clicked on a pair that doesnt match. 
+            {
+                lastTextBlockClicked.Visibility = Visibility.Visible;
+                findingMatch = false;
+            }
+
+        }
+
+        private void TimeTextBlock_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if(matchesFound == 8)
+            {
+                SetUpGame();
             }
         }
     }
